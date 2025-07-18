8e6a3742ef292637ec9fd15aba5b47dd8d953322
AhamedHaseen <your_username@users.noreply.github.com>
2025-07-18 19:37:18 +0530

    Enhance user registration form with improved layout and styling

--- registration.php
+++ registration.php
@@ -11,47 +11,66 @@ require_once 'config.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Registration</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
 </head>
 <body>
 
-<div>
-    <?php 
-        
-    ?>
-</div>
-
-    <div>
-        <form action="registration.php" method="POST">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-3">
-                <h1>User Registration</h1>
-                    <p>Please fill in this form to create an account.</p>
-                <hr class="mb-3">
-                    <label for="firstname">First Name:</label>
-                    <input class="form-control" type="text" id="firstname" name="firstname" required>
-
-                    <label for="lastname">Last Name:</label>
-                    <input class="form-control" type="text" id="lastname" name="lastname" required>
-
-                    <label for="email">Email:</label>
-                    <input class="form-control" type="email" id="email" name="email" required>
-
-                    <label for="phone">Phone Number:</label>
-                    <input class="form-control" type="text" id="phonenumber" name="phonenumber" required>
-
-                    <label for="password">Password:</label>
-                    <input class="form-control" type="password" id="password" name="password" required>
-
-                     <hr class="mb-3">
-
-                    <input class="btn btn-primary" type="submit" id="register" name="create" value="Sign Up" >
-
-                    </div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-lg-6 col-md-8 col-sm-10">
+            <div class="registration-container">
+                <div class="form-header">
+                    <h1><i class="fas fa-user-plus"></i> User Registration</h1>
+                    <p>Please fill in this form to create an account</p>
                 </div>
+                
+                <form action="registration.php" method="POST">
+                    <div class="form-group">
+                        <label class="form-label" for="firstname">
+                            <i class="fas fa-user"></i> First Name
+                        </label>
+                        <input class="form-control" type="text" id="firstname" name="firstname" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="form-label" for="lastname">
+                            <i class="fas fa-user"></i> Last Name
+                        </label>
+                        <input class="form-control" type="text" id="lastname" name="lastname" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="form-label" for="email">
+                            <i class="fas fa-envelope"></i> Email Address
+                        </label>
+                        <input class="form-control" type="email" id="email" name="email" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="form-label" for="phone">
+                            <i class="fas fa-phone"></i> Phone Number
+                        </label>
+                        <input class="form-control" type="text" id="phonenumber" name="phonenumber" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="form-label" for="password">
+                            <i class="fas fa-lock"></i> Password
+                        </label>
+                        <input class="form-control" type="password" id="password" name="password" required>
+                    </div>
+
+                    <hr class="divider">
+
+                    <button class="btn btn-register" type="submit" id="register" name="create">
+                        <i class="fas fa-user-plus"></i> Create Account
+                    </button>
+                </form>
             </div>
-        </form>
+        </div>
     </div>
+</div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
     <!-- Jquery -->
